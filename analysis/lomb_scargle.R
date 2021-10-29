6ee8c12ce83271c717f2b2dd54080d1445d3cc1a
Victor Nagahama <victorh.nagahama@gmail.com>
2021-10-29 10:50:44 +0100

    lomb-scargle initial commit

---
+++ analysis/lomb_scargle.R
@@ -0,0 +1,37 @@
+ls_periodograms <- function(x, y, freqs, centered = T, normalize = T){
+  # R.H.D. Townsend, “Fast calculation of the Lomb-Scargle periodogram using 
+  # graphics processing units.”, The Astrophysical Journal Supplement Series, 
+  # vol 191, pp. 247-253, 2010 - Scipy
+  y_raw <- y
+  if (centered) {
+    y <- y - mean(y)
+  }
+
+  m_x <- matrix(rep(x, length(freqs) * length(x), times = length(freqs)), ncol = length(freqs))
+  m_freqs <- diag(freqs)
+  m <- m_x %*% m_freqs
+  
+  c <- apply(m, MARGIN = 2, cos)
+  s <- apply(m, MARGIN = 2, sin)
+  
+  xc <- colSums(y * c)
+  xs <- colSums(y * s)
+  cc <- colSums(c^2)
+  ss <- colSums(s^2)
+  cs <- colSums(c * s)
+  
+  tau <- 2 * cs / (cc - ss)
+  
+  c_tau <- sapply(freqs * tau, cos)
+  s_tau <- sapply(freqs * tau, sin)
+  
+  pgram <- 0.5 * (
+    (c_tau * xc + s_tau * xs)^2 / (c_tau^2 * cc + 2 * c_tau * s_tau * cs + s_tau^2 * ss) +
+      (c_tau * xs - s_tau * xc)^2 / (c_tau^2 * ss - 2 * c_tau * s_tau * cs + s_tau^2 * cc))
+  
+  if (normalize) {
+    pgram <- 2 * pgram / sum(y_raw^2)
+  }
+  
+  return(pgram)
+}
